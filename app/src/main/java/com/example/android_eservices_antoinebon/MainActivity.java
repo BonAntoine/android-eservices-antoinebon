f6cfd8a594781eb95ebf4db446c6866254c34b4c
Antoine Bon <antoine.bon.etu@univ-lille.fr>
2021-02-13 12:10:17 +0100

    Initial commit - Empty activity

---
+++ app/src/main/java/com/example/android_eservices_antoinebon/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_eservices_antoinebon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
