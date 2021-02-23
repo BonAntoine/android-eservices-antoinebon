53f7f90390b66765f5a2f919ed8fc595340e844e
Antoine Bon <antoine.bon.etu@univ-lille.fr>
2021-02-23 09:34:56 +0100

    feat: add tabs layout and instances

--- app/src/main/java/com/example/android_eservices_antoinebon/MainActivity.java
+++ app/src/main/java/com/example/android_eservices_antoinebon/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android_eservices_antoinebon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import data.api.CharacterApi;
 import data.api.dependencyInjection.DependencyInjection;
 import data.api.model.CharacterSearchResponse;
@@ -13,13 +16,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.charDisplay.fragment.CharListFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Things to do on onCreate when launching main activity of the app
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Init the view pager
+        initViewPager();
+
+        /*
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -44,5 +56,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+         */
+
+    }
+
+    private void initViewPager() {
+        viewPager = findViewById(R.id.main_view_pager);
+
+        CharListFragment charListFragmentInstance = CharListFragment.newInstance();
+        CharListFragment charListFragmentInstanceGrid = CharListFragment.newInstance();
+
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            // Need to create a new instance for each tabs (here 2 instance because 2 tabs)
+            @Override
+            public Fragment getItem(int position) {
+                if (position == 0) {
+                    return charListFragmentInstance;
+                }
+                return charListFragmentInstanceGrid;
+            }
+
+            // I made tab name available in strings to make replace or traduction easier
+            @Override
+            public CharSequence getPageTitle(int position) {
+                if (position == 0) {
+                    return getString(R.string.tab_title_linear);
+                }
+                return getString(R.string.tab_title_grid);
+            }
+
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/charDisplay/fragment/CharListFragment.java
@@ -0,0 +1,12 @@
+package com.example.charDisplay.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class CharListFragment extends Fragment {
+
+    public static CharListFragment newInstance() {
+        return new CharListFragment();
+    }
+
+
+}
